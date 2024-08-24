e64364528b29a3a77f3dcdd347afdbba493e6115
ashikalahi546 <144088399+ashikalahi546@users.noreply.github.com>
2024-08-24 11:51:38 +0600

    json file add

---
+++ src/components/json/JsonFile.js
@@ -0,0 +1,34 @@
+// {
+//     "workbench.iconTheme": "material-icon-theme",
+//     "files.autoSave": "afterDelay",
+//     "[html]": {
+//         "editor.defaultFormatter": "esbenp.prettier-vscode"
+//     },
+ 
+//     "[javascript]": {
+//         "editor.defaultFormatter": "esbenp.prettier-vscode"
+//     },
+//     "editor.stickyScroll.enabled": false,
+//     "editor.minimap.enabled": false,
+//     "editor.fontLigatures": true,
+//     "editor.fontFamily": "'fira code'",
+//     "editor.fontWeight": "400",
+//     "json.schemas": [],
+//     "emmet.includeLanguages": {
+//         "javascript": "javascriptreact"
+//     },
+//     "[json]": {
+//         "editor.defaultFormatter": "esbenp.prettier-vscode"
+//     },
+//     "terminal.integrated.env.windows": {},
+//     "update.showReleaseNotes": false,
+//     "diffEditor.ignoreTrimWhitespace": false,
+//     "editor.fontSize": 13,
+//     "[css]": {
+//         "editor.defaultFormatter": "esbenp.prettier-vscode"
+//     },
+//     "workbench.colorTheme": "Karma",
+//     "console-ninja.featureSet": "Community",
+//     "javascript.updateImportsOnFileMove.enabled": "always"
+    
+// }

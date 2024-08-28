45e0cff907c89726fa8345bebc70ce1777728048
ashikalahi546 <144088399+ashikalahi546@users.noreply.github.com>
2024-08-28 16:05:49 +0600

    Update JsonFile.js

--- src/components/json/JsonFile.js
+++ src/components/json/JsonFile.js
@@ -1,34 +1,40 @@
 // {
-//     "workbench.iconTheme": "material-icon-theme",
-//     "files.autoSave": "afterDelay",
-//     "[html]": {
-//         "editor.defaultFormatter": "esbenp.prettier-vscode"
-//     },
- 
-//     "[javascript]": {
-//         "editor.defaultFormatter": "esbenp.prettier-vscode"
-//     },
-//     "editor.stickyScroll.enabled": false,
-//     "editor.minimap.enabled": false,
-//     "editor.fontLigatures": true,
-//     "editor.fontFamily": "'fira code'",
-//     "editor.fontWeight": "400",
-//     "json.schemas": [],
-//     "emmet.includeLanguages": {
-//         "javascript": "javascriptreact"
-//     },
-//     "[json]": {
-//         "editor.defaultFormatter": "esbenp.prettier-vscode"
-//     },
-//     "terminal.integrated.env.windows": {},
-//     "update.showReleaseNotes": false,
-//     "diffEditor.ignoreTrimWhitespace": false,
-//     "editor.fontSize": 13,
-//     "[css]": {
-//         "editor.defaultFormatter": "esbenp.prettier-vscode"
-//     },
-//     "workbench.colorTheme": "Karma",
-//     "console-ninja.featureSet": "Community",
-//     "javascript.updateImportsOnFileMove.enabled": "always"
-    
+//   "workbench.iconTheme": "material-icon-theme",
+//   "files.autoSave": "afterDelay",
+//   "[html]": {
+//     "editor.defaultFormatter": "esbenp.prettier-vscode"
+//   },
+//   "workbench.colorCustomizations": {
+//     "editorBracketHighlight.foreground2": "#5af78e"
+//   },
+
+//   "[javascript]": {
+//     "editor.defaultFormatter": "esbenp.prettier-vscode"
+//   },
+//   "editor.stickyScroll.enabled": false,
+//   "editor.minimap.enabled": false,
+//   "editor.fontLigatures": true,
+//   "editor.fontFamily": "'fira code'",
+//   "editor.fontWeight": "400",
+//   "json.schemas": [],
+//   "emmet.includeLanguages": {
+//     "javascript": "javascriptreact"
+//   },
+
+//   "[json]": {
+//     "editor.defaultFormatter": "esbenp.prettier-vscode"
+//   },
+//   "terminal.integrated.env.windows": {},
+//   "update.showReleaseNotes": false,
+//   "diffEditor.ignoreTrimWhitespace": false,
+//   "editor.fontSize": 13,
+//   "[css]": {
+//     "editor.defaultFormatter": "esbenp.prettier-vscode"
+//   },
+//   "workbench.colorTheme": "Karma Legacy",
+//   "console-ninja.featureSet": "Community",
+//   "javascript.updateImportsOnFileMove.enabled": "always",
+//   "[jsonc]": {
+//     "editor.defaultFormatter": "esbenp.prettier-vscode"
+//   }
 // }
